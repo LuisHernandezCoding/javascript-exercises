0771289e363a3122398c231f048a9a0dbab7814e
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-09-15 02:43:04 -0500

    done exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+    if (n === 0) return ''
+    else if (n < 0) return 'ERROR'
 
+    if (string === '') return ''
+
+    for (let i = 1; i < n; i++){
+        string += 'hey'
+    }
+    return string
 };
 
 // Do not edit below this line
