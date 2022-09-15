86b28e9c525e625594a8b16afe2be82a672e6aec
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-09-15 18:45:29 -0500

    Exercise 5 from sumAll its Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    if (startNumber < 0 || endNumber < 0 || 
+        typeof startNumber !== "number" || 
+        typeof endNumber !== "number") 
+    return 'ERROR'
+    let numberQuantity, lowerNumber
+    if (startNumber < endNumber) { 
+        numberQuantity = (endNumber - startNumber + 1)
+        lowerNumber = startNumber
+    }
+    else {
+        numberQuantity = (startNumber - endNumber + 1) 
+        lowerNumber = endNumber
+    }
 
+    let sum = 0
+    for (let i = 0; i < numberQuantity; i++) {
+        (i === 0) ? sum = lowerNumber :        
+        sum += (lowerNumber+i)
+    }
+    return sum
 };
 
 // Do not edit below this line
