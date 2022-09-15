684ac62c07bb1e95bbbd518b9bd98c13f0028d65
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-09-15 17:38:32 -0500

    exercise 3, reverseStrings its done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // 
+    let reversedString = ''
+    let charactersArray = []
+    for (let i = 0; i < str.length; i++) {
+        charactersArray.push(str.toString().charAt(i))
+    }
+    charactersArray.reverse()
+    for (let i = 0; i < str.length; i++) {
+        reversedString += charactersArray[i]        
+    }
+    return reversedString
 };
 
 // Do not edit below this line
