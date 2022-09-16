52ea80c310850dede5f63bbeee921df053eb4fa9
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-09-15 19:09:43 -0500

    Done Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  // [°C] = ([°F] − 32) × 5⁄9
+  let c = ((f - 32) * 5) / 9
+  return round(c, 1)
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  // [°F] = [°C] × 9⁄5 + 32
+  let f = (c * (9/5)) + 32
+  return round(f, 1)
 };
+
+const round = function(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
 
 // Do not edit below this line
 module.exports = {
