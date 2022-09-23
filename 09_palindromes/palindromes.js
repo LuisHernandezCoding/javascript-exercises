3b5c287323d033cc7cf5a6a8f00b5abda2abfbf3
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-09-23 18:23:09 -0500

    done palindromes exercise, with 2 solutions, the one that is not commented its the final, i tried to get a cleaner and more elegant solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const normalWord = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+    const inverseWord = normalWord.split('').reverse().join('')
+    if (normalWord === inverseWord) return true; else return false
+    
+    /*
+    const initialStr = str.split(' ').join('').split('.').join('').split('!').join('').split(',').join('').toLowerCase().split('');
+    const reversedStr = str.split(' ').join('').split('.').join('').split('!').join('').split(',').join('').toLowerCase().split('').reverse()
+    for (let i = 0; i < initialStr.length / 2; i++) {
+        if (initialStr[i] != reversedStr[i]) return false
+    }
+    return true
+    */
 
-};
 
+}
 // Do not edit below this line
 module.exports = palindromes;
