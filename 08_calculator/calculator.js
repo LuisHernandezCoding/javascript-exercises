f98cf41363fa542d0a42c9de39b825441e2f0ded
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-09-23 03:12:04 -0500

    Done exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) { 
+  if(array.reduce((a, b) => a + b, -1) === -1) return 0;
+  else return array.reduce((a, b) => a + b)
+//  let result = 0
+//  for (let i = 0; i < array.length; i++) result += array[i]
+//  return result  
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if(array.reduce((a, b) => a * b, -1) === -1) return 0;
+  else return array.reduce((a, b) => a * b)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0){
+    return 1
+  }
+	return a * factorial(a-1)
 };
 
 // Do not edit below this line
