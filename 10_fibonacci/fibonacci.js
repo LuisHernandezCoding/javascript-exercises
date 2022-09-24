52a0c6482ed255a7b95e9e73ac30f8fd92f5c030
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-09-23 19:05:09 -0500

    Finished fibonacci exercise Added 2 solutions, one using numbers only as 3 variables actual, old and final and another using arrays and storing each fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    /*
+    let oldNumber = 1, actualNumber = 1, finalNumber = 1
+    if (n < 0) return 'OOPS'
+    for (let i = 1; i < n; i++) {
+        if (i >= 2){
+            oldNumber = finalNumber
+            finalNumber += actualNumber
+            actualNumber = oldNumber
+        }        
+    }
+    return finalNumber
+    */
 
+    let numbersArray = [0, 1, 1]
+    if (n < 0) return 'OOPS'
+    for (let i = 0; i < n; i++) { 
+        if (i >= 2){numbersArray.push(numbersArray[i] + numbersArray[i-1])}
+    }
+    return numbersArray[n]
 };
 
 // Do not edit below this line
