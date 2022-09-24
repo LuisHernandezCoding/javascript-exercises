f8e1702b6f9c791beff1822d29e414bd54cf806c
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-09-23 19:59:57 -0500

    done Find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const orderedPeople = people.sort((a,b) => 
+    (a.yearOfBirth - ((typeof a.yearOfDeath === 'undefined') 
+                    ? new Date().getFullYear() : a.yearOfDeath)) > 
+    (b.yearOfBirth - ((typeof b.yearOfDeath === 'undefined') 
+    ? new Date().getFullYear() : b.yearOfDeath))
+    ? 1 : -1)
+    return orderedPeople[0]
 };
 
 // Do not edit below this line
